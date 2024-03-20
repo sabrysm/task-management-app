6b96e43753e21725a9c3d245278bf9bd4a785137
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-03-20 14:20:17 +0200

    login required for tasks history page

--- task_manager/reporting/views.py
+++ task_manager/reporting/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import TaskHistory
 from django.utils import timezone
 
+@login_required
 def tasks_history(request):
     # Get Tasks completed
     completed = TaskHistory.objects.filter(user=request.user).order_by('-completed_at')
