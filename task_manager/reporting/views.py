8ec9c77e7e04784122b3cd615fd29e500c9aee6e
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-03-25 00:36:33 +0200

    Added scrollbar for the tasks history page

--- task_manager/reporting/views.py
+++ task_manager/reporting/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import CompletedTask
 from django.utils import timezone
@@ -9,9 +8,6 @@ from django.utils import timezone
 def tasks_history(request):
     # Get Tasks completed
     completed = CompletedTask.objects.filter(user=request.user).order_by('-completed_at')
-    paginator = Paginator(completed, 10)
-    page = request.GET.get('page')
-    completed = paginator.get_page(page)
     context = {
         "page": completed,
     }
